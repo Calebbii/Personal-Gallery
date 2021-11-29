e76a20728aa12a0930d29d6f61c7b84267cb3af7
Calebbii <biicaleb316@gmail.com>
2021-11-29 11:49:10 +0300

    updates url patterns

---
+++ gallery/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import include, url
+from django.db.models.query_utils import PathInfo
+from django.urls.conf import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url('^$',views.home,name = 'welcome'),
+    path('photos/',views.images,name='allPhotos'),
+    path('photos/',views.images,name='allVideo'),
+    path('photos/<int:image_id>',views.detail,name='image_item.detail'),
+    path('nature/',views.filter_nature_images,name='nature'),
+    path('food&drinks/',views.filter_food_drinks_images,name='foods'),
+    path('wildlife/',views.filter_wildlife_images,name='wildlife'),
+    path('travels/',views.filter_travels_images,name='travels'),
+    path('details/',views.images,name='details'),
+    path('search/',views.search_results, name='search_results')
+
+]
+if settings.DEBUG:
+    # urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # urlpatterns += url(r'',include('users.urls'))
