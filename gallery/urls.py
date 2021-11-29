ba1d03870a4107f47b83e86150867a17416b09bd
Calebbii <biicaleb316@gmail.com>
2021-11-29 20:12:24 +0300

    commit adds templates

--- gallery/urls.py
+++ gallery/urls.py
@@ -9,14 +9,13 @@ urlpatterns=[
     url('^$',views.home,name = 'welcome'),
     path('photos/',views.images,name='allPhotos'),
     path('photos/',views.images,name='allVideo'),
-    path('photos/<int:image_id>',views.detail,name='image_item.detail'),
+    path('image/<int:image_id>',views.detail,name='image_item.detail'),
     path('nature/',views.filter_nature_images,name='nature'),
     path('food&drinks/',views.filter_food_drinks_images,name='foods'),
     path('wildlife/',views.filter_wildlife_images,name='wildlife'),
     path('travels/',views.filter_travels_images,name='travels'),
+    path('search/',views.search_results, name='search_results'),
     path('details/',views.images,name='details'),
-    path('search/',views.search_results, name='search_results')
-
 ]
 if settings.DEBUG:
     # urlpatterns += staticfiles_urlpatterns()
--- gallery/views.py
+++ gallery/views.py
@@ -19,12 +19,24 @@ def detail(request,image_id):
     images = get_object_or_404(Image, pk = image_id)
   except ObjectDoesNotExist:
     raise Http404()
-  return render(request, 'photo.html', {'images':images,"locations":locations})
+  return render(request, 'details.html', {'images':images,"locations":locations})
 
 
 def search_results(request):
-    if 'image' in request.GET and request.GET["image"]:
-        search_term = request.GET.get("image")
+    if 'search' in request.GET and request.GET["search"]:
+        search_term = request.GET.get("search")
+        searched_images = Image.search_images_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "Please enter Category to be searched"
+        return render(request, 'search.html',{"message":message})
+
+def search_title(request):
+    if 'search' in request.GET and request.GET["search"]:
+        search_term = request.GET.get("search")
         searched_images = Image.search_by_title(search_term)
         message = f"{search_term}"
 
@@ -64,3 +76,10 @@ def filter_travels_images(request):
   except ObjectDoesNotExist:
     raise Http404()
   return render(request, 'imageInfo.html', {'images':images})
+
+def image(request,article_id):
+    try:
+        image = Image.objects.get(id = article_id)
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request,"details.html", {"image":image})
